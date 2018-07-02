6696c2511207f7f78e0b6c01071d84a251ebdca8
Nikolaj Kaare Nørskov <nkn@reviso.com>
2018-07-02 10:38:52 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,48 @@
+
+require('dotenv').config();
+let express = require('express');
+let request = require('request');
+let app = express();
+
+let restUrl = process.env.RESTURL;
+let appSecret = process.env.APPSECRET;
+let exchangeUrl = restUrl + 'apps-auth/grant-token/';
+
+let baseHeaders = {
+	'X-AppSecretToken': appSecret
+};
+
+app.get('/:value', (req, res) => {
+	var value = req.params.value;
+	var eaToken = req.query.embeddedAppToken;
+
+	var exchangeForGrant = {
+		url: exchangeUrl + eaToken,
+		headers: baseHeaders		
+	};
+
+	request(exchangeForGrant, (error, response, body) => {
+		if (error || response.statusCode != 200) {
+			console.log('exchange error', error, body);
+		}
+
+		let grantToken = body;
+		console.log('exchanged for grantToken: ' + grantToken);
+		
+		var getCompanyInfo = {
+			url: restUrl + 'self/company',
+			headers: Object.assign({}, baseHeaders, { 'X-AgreementGrantToken' : grantToken })	
+		}
+		request(getCompanyInfo, (subError, subResponse, subBody) => {
+			if (subError || subResponse.statusCode != 200) {
+				console.log('company error', subError, response);
+				return;
+			}
+
+			let companyInfo = JSON.parse(subBody);
+			res.json(companyInfo);
+		});
+	});
+}); 
+
+app.listen(3000, () => console.log('Example app listening on port 3000'));
